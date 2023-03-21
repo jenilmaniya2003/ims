0ebc008733321a83761636eabcdeec423a35f7aa
AnshGalani2003 <anshgalani@yahoo.com>
2023-03-21 12:14:40 +0530

    First

--- ledger/ledger.php
+++ ledger/ledger.php
@@ -1,128 +1,142 @@
-<?php
-session_start();
-if (strlen($_SESSION['aid'] == 0)) {
-    header('location:../index.php');
-} else {
+<?php 
+include('../inc/connection.php');
+include('../inc/menu.php');
 
-    include('../inc/connection.php');
-    include('../inc/menu.php');
+$sql1 = "SELECT party.name, ledger_trans.id, ledger_trans.cid,ledger_trans.challan_no,ledger_trans.received_amount
+FROM ledger_trans
+JOIN party ON party.id = ledger_trans.cid
+GROUP BY ledger_trans.cid;";
+$res1 = mysqli_query($con, $sql1);
 
-
+// $sql2="select party.name,ledger_trans.cid from ledger_trans join party on party.id=ledger_trans.cid;";
+// $res2=mysqli_query($con,$res2);
 ?>
-    <!DOCTYPE html>
+<!DOCTYPE html>
     <html lang="en">
 
     <head>
-        <meta charset="utf-8">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>Avalon Metalic</title>
         <link rel="icon" href="../IMG/logo.png" type="image/x-icon">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/css/datatables.min.css">
-        <link rel="stylesheet" href="../assets/css/style.css">
-        <link rel="stylesheet" href="../assets/buttons.dataTables.min.css">
-
-        <script src="../assets/js/jquery-3.6.0.min.js"></script>
-        <script src="../assets/js/jquery.dataTables.min.js"></script>
-        <script src="../assets/js/dataTables.buttons.min.js"></script>
-        <script src="../assets/js/jszip.min.js"></script>
-        <script src="../assets/js/buttons.html5.min.js"></script>
-        <script src="../assets/js/buttons.colVis.min.js"></script>
-
-        <style>
-            .img1 {
-                position: fixed;
-                bottom: 60px;
-                right: 60px;
-            }
-        </style>
-
+        <link href="../vendors/jquery-toggles/css/toggles.css" rel="stylesheet" type="text/css">
+        <link href="../vendors/jquery-toggles/css/themes/toggles-light.css" rel="stylesheet" type="text/css">
+        <link href="../dist/css/style.css" rel="stylesheet" type="text/css">
     </head>
 
     <body>
-        <div class="container ">
-            <!-- <a href="add_cust.php">
-            <img src="../IMG/add.png" height="40px" width="40px" alt="" class="img1">
-        </a> -->
-            <div class="data_table">
-                <form method="post">
-                    <table id="example" class="table table-striped table-bordered">
-                        <thead class="table-dark">
-                            <tr>
-                                <!-- <th style="width: 90px; text-align:center;">
-                                    <button type="submit" name="prod_delete_multiple_btn" class="btn btn-danger">Delete</button>
-                                    <input type="checkbox" id="chkAll">
-                                </th> -->
-                                <th>Credit</th>
-                                <th>Date</th>
-                                <th>Description</th>
-                                <th>Debit</th>
-                                <th>Date</th>
-                                <th>Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $cnt = 1;
-                            $query = "select supplier.name,purch_trans.id,purch_trans.invoice_no,purch_trans.pending_amount,purch_trans.total_amount,purch_trans.sid from purch_trans join supplier on supplier.id=purch_trans.sid;";
-                            $res = mysqli_query($con, $query);
-                            while ($row = mysqli_fetch_array($res)) {
-                                if ($row['pending_amount'] > 0) {
+    <div class="hk-wrapper">
 
-                                    // foreach ($res as $row) {
-                            ?>
-                                    <tr>
-                                        <!-- <td style="width: 10px; text-align:center;">
-                                            <input type="checkbox" name="product_delete_id[]" value="<?= $row['id']; ?>" class="tblChk">
-                                        </td> -->
-                                        <td style="text-align: center;" class="see"><?= $cnt; ?></td>
-                                        <td style="text-align: center;"><?= $row['invoice_no']; ?></td>
-                                        <td style="text-align: center;"><?= $row['name']; ?></td>
-                                        <td style="text-align: center;"><?= $row['total_amount']; ?></td>
-                                        <td style="text-align: center;"><?= $row['pending_amount']; ?></td>
+<section class="hk-sec-wrapper">
 
-                                        <td>
-                                            <a href="updt_purchase_pen_trans.php?id=<?php echo $row['id']; ?>"><img src="../IMG/edit.png" height="25px" width="30PX"></a>&nbsp;
-                                            <a href="del_purchase_pen_trans.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Are You Sure??');"><img src="../IMG/delete.png" height="20px" width="20px"></a>&nbsp;
-                                        </td>
-                                    </tr>
-                            <?php
-                                    $cnt++;
-                                }
-                            }
-                            ?>
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <form class="needs-validation" method="post" novalidate>
 
-                        </tbody>
-                    </table>
-                </form>
-            </div>
-        </div>
-        <script>
-            $(document).ready(function() {
-                $("#chkAll").change(function() {
-                    debugger;
-                    if ($(this).prop('checked')) {
-                        $('.tblChk').not(this).prop('checked', true);
-                    } else {
-                        $('.tblChk').not(this).prop('checked', false);
-                    }
-                    // getCheckRecords();
-                })
-            });
-        </script>
+                                            <div class="form-row">
+                                                <div class="col-md-6 mb-10">
+                                                    <!-- <label for="validationCustom03">Product Name</label>
+                <input type="text" class="form-control" id="validationCustom03" placeholder="Product Name" name="customername" required>
+                <div class="invalid-feedback">Please provide a valid product name.</div> -->
+                                                    <select name="customername" id="customername" class="form-control custom-select">
+                                                        <option value="">Select Customer</option>
+                                                        <?php
+                                                        while ($id = mysqli_fetch_array($res1)) {
+                                                        ?>
+                                                            <option value="<?php echo $id['cid']; ?>"> <?php echo $id['name']; ?>
+                                                            </option>
+                                                        <?php
+                                                        }
+                                                        ?>
+                                                    </select>
+                                                </div>
+                                            </div>
 
-        <script src="../assets/js/bootstrap.bundle.min.js"></script>
-        <script src="../assets/js/datatables.min.js"></script>
-        <script src="../assets/js/pdfmake.min.js"></script>
-        <script src="../assets/js/vfs_fonts.js"></script>
-        <script src="../assets/js/custom.js"></script>
 
+                                            <button class="btn btn-primary" type="submit" name="search">search</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </section>
+                            <!--code for search result -->
+                            <?php if (isset($_POST['search'])) { ?>
+                                <section class="hk-sec-wrapper">
+
+                                    <div class="row">
+                                        <div class="col-sm">
+                                            <div class="table-wrap">
+                                                <table id="datable_1" class="table table-hover w-100 display pb-30">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>#</th>
+                                                            <th>Credit</th>
+                                                            <th>Date</th>
+                                                            <th>Description</th>
+                                                            <th>Debit</th>
+                                                            <th>Date</th>
+                                                            <th>Challan No</th>
+                                                            <!-- <th>Product</th> -->
+                                                            <!-- <th>Pricing</th> -->
+
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php
+
+                                                        $cnt = 1;
+                                                        $name = $_POST['customername'];
+                                                        // $ledger_qry= $con->query("select party.name,ledger_trans.id,ledger_trans.challan_no,ledger_trans.cid,ledger_trans.received_amount,ledger_trans.date from ledger_trans right join party on ledger_trans.cid=party.id where cid='$name'");
+                                                        // $ledger=$ledger_qry->fetch_array();
+                                                        $query="select party.name,ledger_trans.id,ledger_trans.challan_no,ledger_trans.cid,ledger_trans.received_amount,ledger_trans.date from ledger_trans right join party on ledger_trans.cid=party.id where cid=$name";
+                                                        $res = mysqli_query($con, $query);
+                                                        while ($row = mysqli_fetch_array($res)) {
+                                                            
+                                                        ?>
+                                                            
+                                                                <tr>
+                                                                    <td><?php echo $cnt;?></td>
+                                                                    <td><?php echo $row['received_amount']; ?></td>
+                                                                    <td><?php echo $row['date']; ?></td>
+                                                                    <td>Cash</td>
+                                                                    <td>Cash</td>
+                                                                    <td>Cash</td>
+                                                                    <?php 
+                                                                    $total=$row['cid'];
+                                                                    $q1 = "select sum(amount) as tt  from sales where party_name=$total;";
+                                                                    $r1 = mysqli_query($con, $q1);
+                                                                    $row1 = mysqli_fetch_array($r1);
+                                                                    ?>
+                                                                    <td><?php echo $row1['tt']; ?></td>
+                                                                    
+                                                                </tr>
+
+                                                            
+                                                        <?php
+                                                            $cnt++;
+                                                        } ?>
+
+                                                      
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                            <?php } ?>
+
+    </div>
+    
     </body>
-
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+        <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
+        <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="vendors/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
+        <script src="dist/js/jquery.slimscroll.js"></script>
+        <script src="dist/js/dropdown-bootstrap-extended.js"></script>
+        <script src="dist/js/feather.min.js"></script>
+        <script src="vendors/jquery-toggles/toggles.min.js"></script>
+        <script src="dist/js/toggle-data.js"></script>
+        <script src="dist/js/init.js"></script>
+        <script src="dist/js/validation-data.js"></script>
     </html>
-
-<?php
-}
-include('../inc/footer.php');
-?>
